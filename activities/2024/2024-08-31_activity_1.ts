132fd7ae0041d392a7b973990d4abd4b1c388e47
Tamicktom <rickpokemon38@gmail.com>
2024-08-31 12:38:28 -0300

    ✨ TypeScript: Atividade #2 em 31/08/2024

---
+++ activities/2024/2024-08-31_activity_1.ts
@@ -0,0 +1,35 @@
+/**
+ * Refatorando código 🔧
+ * Data: 31/08/2024
+ * Commit #1
+ */
+
+interface DailyActivity {
+    date: string;
+    activity: string;
+    completed: boolean;
+}
+
+class ActivityTracker {
+    private activities: string[] = [
+        "Tipando interfaces TypeScript",
+        "Implementando design patterns",
+        "Corrigindo tipos e generics",
+        "Configurando ESLint/Prettier",
+        "Escrevendo testes com Jest",
+        "Refatorando para clean code"
+    ];
+
+    public logActivity(): DailyActivity {
+        return {
+            date: "31/08/2024",
+            activity: "Refatorando código 🔧",
+            completed: true
+        };
+    }
+}
+
+const tracker = new ActivityTracker();
+console.log(tracker.logActivity());
+
+export { ActivityTracker };
